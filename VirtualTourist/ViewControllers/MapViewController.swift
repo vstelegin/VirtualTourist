18523c39a9689987d5dbeaccfb730ea1aa94ef73
Chase Cartman <vstelegin@gmail.com>
2018-06-19 23:59:32 +0300

    test pin placement

--- VirtualTourist/ViewControllers/MapViewController.swift
+++ VirtualTourist/ViewControllers/MapViewController.swift
@@ -7,20 +7,34 @@
 //
 
 import UIKit
+import MapKit
 
-class MapViewController: UIViewController {
+class MapViewController: UIViewController, MKMapViewDelegate{
     var  albumViewActive = false
+    var annotation : MKPointAnnotation? = nil
+    
+    @IBOutlet weak var mapView: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        mapView.delegate = self
+        
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
     @IBAction func addPin (_ sender: UILongPressGestureRecognizer) {
+        let location = sender.location(in: mapView)
+        let coordinate = mapView.convert(location, toCoordinateFrom: mapView)
+        
+        if sender.state == .began {
+            annotation = MKPointAnnotation()
+            annotation?.coordinate = coordinate
+            mapView.addAnnotation(annotation!)
+        }
         if !albumViewActive{
             performSegue(withIdentifier: "showAlbum", sender: nil)
             print ("seque")
